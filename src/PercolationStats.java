141faee805f94fae6e8659e641f3e709cd5c78ec
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-29 17:54:54 +0200

    First Commit

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -4,28 +4,55 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
     // perform independent trials on an n-by-n grid
+    double[] fractionOfOpenSites;
     private int n;
     private int trials;
 
-    public PercolationStats(int n, int trials){
-        this.n=n;
-        this.trials=trials;
+    public PercolationStats(int n, int trials) {
+        this.n = n;
+        this.trials = trials;
+        fractionOfOpenSites=new double[trials];
+
+        for (int i =0; i<trials;i++){
+            Percolation percolation=new Percolation(n);
+            int openSites=0;
+           while (!percolation.percolates()){
+               int row=StdRandom.uniformInt(n)+1;
+               int col=StdRandom.uniformInt(n)+1;
+               if (!percolation.isOpen(row,col)){
+                   percolation.open(row,col);
+                   openSites++;
+               }
+           }
+           fractionOfOpenSites[i]=(double) openSites/(double) (n*n);
+        }
+
+
+
     }
 
     // sample mean of percolation threshold
-    public double mean(){
-
+    public double mean() {
+        return 0;
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev()
+    public double stddev(){
+        return 0;
+    }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo()
+    public double confidenceLo(){
+        return 0;
+    }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi()
+    public double confidenceHi(){
+        return 0;
+    }
 
     // test client (see below)
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+    }
 }
