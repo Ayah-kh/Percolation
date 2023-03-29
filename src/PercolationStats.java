440ca5d0327dd616ddd9721515a20a226bdf23eb
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-29 16:45:49 +0200

    First Commit

---
+++ src/PercolationStats.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    // perform independent trials on an n-by-n grid
+    private int n;
+    private int trials;
+
+    public PercolationStats(int n, int trials){
+        this.n=n;
+        this.trials=trials;
+    }
+
+    // sample mean of percolation threshold
+    public double mean(){
+
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev()
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo()
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi()
+
+    // test client (see below)
+    public static void main(String[] args)
+}
