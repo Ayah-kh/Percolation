3c892250c8afe32ce53a1c1e9042864945d53b61
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-29 17:59:58 +0200

    First Commit

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
@@ -5,12 +6,9 @@ import edu.princeton.cs.algs4.StdStats;
 public class PercolationStats {
     // perform independent trials on an n-by-n grid
     double[] fractionOfOpenSites;
-    private int n;
-    private int trials;
 
     public PercolationStats(int n, int trials) {
-        this.n = n;
-        this.trials = trials;
+
         fractionOfOpenSites=new double[trials];
 
         for (int i =0; i<trials;i++){
@@ -33,26 +31,36 @@ public class PercolationStats {
 
     // sample mean of percolation threshold
     public double mean() {
-        return 0;
+        return StdStats.mean(fractionOfOpenSites);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev(){
-        return 0;
+        return StdStats.mean(fractionOfOpenSites);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo(){
-        return 0;
+        return mean()-1.96*stddev()/Math.sqrt(fractionOfOpenSites.length);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi(){
-        return 0;
+        return mean()+1.96*stddev()/Math.sqrt(fractionOfOpenSites.length);
     }
 
     // test client (see below)
     public static void main(String[] args){
+
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(n, trials);
+
+        StdOut.println("mean                    = " + stats.mean());
+        StdOut.println("stddev                  = " + stats.stddev());
+        StdOut.println("95% confidence interval = "
+                + stats.confidenceLo() + ", "
+                + stats.confidenceHi());
 
     }
 }
